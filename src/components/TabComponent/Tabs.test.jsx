f718c0bdd67bc62e43ab2e693f5ea3f7fea26f8b
Gab Erves <gerves20@gmail.com>
2023-03-29 14:42:52 -0400

    testing scripts

--- src/components/TabComponent/Tabs.test.jsx
+++ src/components/TabComponent/Tabs.test.jsx
@@ -1,5 +1,20 @@
 import { describe, expect, test, vi, beforeEach } from 'vitest';
 import { render, screen, fireEvent } from '@testing-library/react';
-import Rates from './Tabs';
+import Tabs from './Tabs';
+
+describe("Navbar rendered with the default theme and tailwind configurations", () => {
+
+    const mockFunction = vi.fn(() => true);
+
+    beforeEach(() => {
+        render(<Tabs onClick={mockFunction}><li>Rates</li></Tabs>)
+    })
+
+    test("Tabs have children, even if they are wrapped in HTML components", () => {
+        const button = screen.getByText(/Rates/i).closest('div');
+        expect(button.innerHTML).toContain('li');
+    })
 
 
+
+})
